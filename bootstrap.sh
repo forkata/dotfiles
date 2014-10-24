f689f1a8c6306e70f68ee503b24c598fd9134adf
Chris Todorov <chris@freerunningtechnologies.com>
2014-10-23 22:01:32 -0700

    Better bootstrap script.
    Symlink dotfiles instead of rsync'ing them.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
+
+cd "$( dirname "$0" )"
 git pull origin master
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-				--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.zshrc
+
+function bootstrap() {
+	# zsh
+	ln -sf "$(pwd)/.zprezto" "$HOME/.zprezto"
+	ln -sf "$(pwd)/.zlogin" "$HOME/.zlogin"
+	ln -sf "$(pwd)/.zlogout" "$HOME/.zlogout"
+	ln -sf "$(pwd)/.zprofile" "$HOME/.zprofile"
+	ln -sf "$(pwd)/.zpreztorc" "$HOME/.zpreztorc"
+	ln -sf "$(pwd)/.zshenv" "$HOME/.zshenv"
+	ln -sf "$(pwd)/.zshrc" "$HOME/.zshrc"
+
+	# git
+	ln -sf "$(pwd)/.gitconfig" "$HOME/.gitconfig"
+	ln -sf "$(pwd)/.gitignore_global" "$HOME/.gitignore_global"
+
+	# dev
+	ln -sf "$(pwd)/.bundle" "$HOME/.bundle"
+	ln -sf "$(pwd)/.ackrc" "$HOME/.ackrc"
+	ln -sf "$(pwd)/.agignore" "$HOME/.agignore"
+
+	# vim
+	ln -sf "$(pwd)/.vimrc" "$HOME/.vimrc"
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	bootstrap
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
+		bootstrap
 	fi
 fi
-unset doIt
+unset bootstrap
