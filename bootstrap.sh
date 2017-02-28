c7e93461682dbe440f09ca3f66a84c5e3082f553
Chris Todorov <chris@freerunningtechnologies.com>
2017-02-27 22:52:36 -0800

    Remove more Prezto cruft

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,6 @@ git pull origin master
 
 function bootstrap() {
   # zsh
-  ln -sf "$(pwd)/.zlogin" "$HOME/.zlogin"
-  ln -sf "$(pwd)/.zlogout" "$HOME/.zlogout"
-  ln -sf "$(pwd)/.zprofile" "$HOME/.zprofile"
   ln -sf "$(pwd)/.zshenv" "$HOME/.zshenv"
   ln -sf "$(pwd)/.zshrc" "$HOME/.zshrc"
   ln -sf "$(pwd)/.zsh" "$HOME/.zsh"
