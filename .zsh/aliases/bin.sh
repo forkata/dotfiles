006b2cb8d17b88cef470e2595737b9afe26c03b2
Chris Todorov <chris@freerunningtechnologies.com>
2017-08-15 22:59:51 -0700

    Update .zsh stuff

--- .zsh/aliases/bin.sh
+++ .zsh/aliases/bin.sh
@@ -1,6 +1,7 @@
 #
 # Some aliases
 #
+alias mkdir="mkdir -p -v"
 alias ls="ls --group-directories-first --color=auto"
 alias xclip="xclip -selection c -i"
 alias vssh="vagrant ssh"
@@ -9,13 +10,14 @@ alias xml="xmllint --format -"
 alias chrome="google-chrome-stable"
 alias update_ubuntu="sudo apt-get update && sudo apt-get upgrade"
 alias ag="rj"
+alias docker-stembolt-ci="docker pull registry.stembolt.io/ci"
 
 # Rails/Ruby aliases
 alias be="bundle exec"
 alias pspec='RAILS_ENV=test be rake parallel:spec'
 
 # Scrot
-alias s='scrot -s -e "mv \$f ~/Screenshots/"'
+alias s='DISPLAY=:0 scrot -s -e "mv \$f ~/Screenshots/"'
 
 # Use hub
 eval "$(hub alias -s)"
--- .zshrc
+++ .zshrc
@@ -2,6 +2,8 @@ autoload -Uz compinit promptinit
 compinit
 promptinit
 
+setopt autocd
+
 # Dir colors.
 LS_COLORS='rs=0:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32:';
 export LS_COLORS
